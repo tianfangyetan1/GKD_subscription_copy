4586b7ab4b5cece4bc76c3d83668181d4819528e
Lin-arm <l2979007648@163.com>
2026-02-14 01:13:18 +0800

    perf: 用药助手

--- src/apps/cn.dxy.medicinehelper.ts
+++ src/apps/cn.dxy.medicinehelper.ts
@@ -10,27 +10,29 @@ export default defineGkdApp({
       rules: [
         {
           fastQuery: true,
-          activityIds: 'cn.dxy.medicinehelper.activity.MainComposeActivity',
-          matches: '[vid="blankForClose"] + [vid="iv_close"]',
+          activityIds: '.activity.MainComposeActivity',
+          matches: [
+            '[text*="新版本" || text$="更新"][visibleToUser=true]',
+            '[vid="iv_close"][clickable=true]',
+          ],
           snapshotUrls: 'https://i.gkd.li/i/25292995',
         },
       ],
     },
     {
       key: 2,
-      name: '分段广告-自动领取每日叮当奖励',
+      name: '功能类-自动领取每日叮当奖励',
       desc: '点击带有叮当+x的去学习并返回',
       rules: [
         {
           key: 0,
-          activityIds:
-            'cn.dxy.medicinehelper.article.dailylearn.DailyLearnComposeActivity',
-          matches: '[text="去学习"] + [text^="+"][text$="丁当"]',
+          activityIds: '.article.dailylearn.DailyLearnComposeActivity',
+          matches:
+            '@[clickable=true] > [text="去学习"] + [text^="+"][text$="丁当"]',
           snapshotUrls: 'https://i.gkd.li/i/25246837',
         },
         {
           preKeys: [0],
-          key: 1,
           activityIds:
             'cn.dxy.drugscomm.business.guide.simple.GuideSimpleComposeActivity',
           matches: 'View[childCount=5][clickable=false][visibleToUser=true]',
@@ -41,35 +43,38 @@ export default defineGkdApp({
     },
     {
       key: 3,
-      name: '局部广告-自动展开详细信息',
+      name: '功能类-自动展开详细信息',
       desc: '药品说明书点击[展开]',
+      activityIds: [
+        '.article.dailylearn.DailyLearnComposeActivity',
+        'cn.dxy.drugscomm.business.ebm.EbmContentComposeActivity',
+        'cn.dxy.drugscomm.business.drug.detail.DrugDetailComposeActivity',
+      ],
       rules: [
         {
-          key: 0,
-          activityIds: [
-            '.article.dailylearn.DailyLearnComposeActivity',
-            'cn.dxy.drugscomm.business.ebm.EbmContentComposeActivity',
-            'cn.dxy.drugscomm.business.drug.detail.DrugDetailComposeActivity',
-          ],
-          matches: ['[text="展开"][visibleToUser=true]'],
+          key: 0, //若有clickable则先点clickable的
+          matches: '@[clickable=true] > [text="展开"][visibleToUser=true]',
           snapshotUrls: [
             'https://i.gkd.li/i/25246982', //展开完整信息
-            'https://i.gkd.li/i/25247361',
             'https://i.gkd.li/i/25247525',
           ],
+        },
+        {
+          key: 1,
+          matches: '[text="展开"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/25247361',
         },
       ],
     },
     {
       key: 4,
-      name: '功能类-药品说明书-表格全屏自动横屏观看',
-      desc: '全屏表格自动横屏观看',
+      name: '功能类-自动横屏观看表格',
+      desc: '药品说明书表格 点击[横屏]',
       rules: [
         {
-          key: 0,
           activityIds:
             'cn.dxy.drugscomm.business.drug.detail.DrugDetailComposeActivity',
-          matches: '@TextView[text="横屏"]',
+          matches: 'TextView[text="横屏"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/25247591',
         },
       ],
@@ -79,28 +84,41 @@ export default defineGkdApp({
       name: '全屏广告',
       rules: [
         {
-          key: 0,
-          activityIds:
-            'cn.dxy.drugscomm.business.medadviser.detail.MedAdviserDetailComposeActivity',
-          matches:
-            '[text*="有帮助吗"] + [text*="参与反馈"] +n ImageView[clickable=true][width<100 && height<100]',
-          snapshotUrls: 'https://i.gkd.li/i/25247634',
-        },
-        {
-          key: 1,
-          activityIds:
-            'cn.dxy.drugscomm.business.ebm.EbmContentComposeActivity',
-          matches:
-            '@ImageView[clickable=true][visibleToUser=true][width<102 && height<102] - View < View[childCount=2]',
-          snapshotUrls: 'https://i.gkd.li/i/25247624',
-        },
-        {
-          key: 2,
+          fastQuery: true,
           activityIds:
             'cn.dxy.drugscomm.business.guide.detail.GuideDetailComposeActivity',
           matches:
-            '@ImageView[desc="用药助手"][clickable=true][visibleToUser=true][width<102 && height<102] - ImageView < View[childCount=5]',
+            '[text^="开通会员后"] -n @ImageView[clickable=true][width<100] <2 View <<4 [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/25247659',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '局部广告',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds:
+            'cn.dxy.drugscomm.business.ebm.EbmContentComposeActivity',
+          matches:
+            '@ImageView[clickable=true] <2 View[index=parent.childCount.minus(1)] <n View <<2 ComposeView < [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/25247624',
+        },
+      ],
+    },
+    {
+      key: 7,
+      name: '评价提示',
+      desc: '问是否有帮助 x掉',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds:
+            'cn.dxy.drugscomm.business.medadviser.detail.MedAdviserDetailComposeActivity',
+          matches:
+            '@ImageView[clickable=true] -n TextView[text*="有帮助吗"] <2 View <<3 ComposeView < [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/25247634',
         },
       ],
     },
