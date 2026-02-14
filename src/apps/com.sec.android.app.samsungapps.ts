1c16cb85d396f31453aebe8344d20f5bc7f2add3
ezn24 <62716651+ezn24@users.noreply.github.com>
2026-02-14 10:27:27 +0800

    feat: 三星应用商店 局部广告-应用推荐广告 (#5)
    * feat: 三星应用商店 局部广告-底部广告（国际版）
    
    * perf: update com.sec.android.app.samsungapps.ts
    
    * perf: com.sec.android.app.samsungapps.ts
    
    * perf: 优化
    
    Signed-off-by: Ling0402 <cjy081219@Gmail.com>
    
    * chore(actions): check_format_lint
    
    * perf: 改为全屏广告
    
    ---------
    
    Signed-off-by: Ling0402 <cjy081219@Gmail.com>
    Co-authored-by: 1030283726 <1030283726@qq.com>
    Co-authored-by: Ling0402 <cjy081219@Gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Lin-arm <l2979007648@163.com>

--- src/apps/com.sec.android.app.samsungapps.ts
+++ src/apps/com.sec.android.app.samsungapps.ts
@@ -54,5 +54,20 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 3,
+      name: '全屏广告-应用推荐广告',
+      desc: '点击[今天不要再显示]',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.SamsungAppsMainActivity',
+          matches:
+            '@[clickable=true] > [vid="do_not_show_again_area_text"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/22320803',
+          exampleUrls: 'https://e.gkd.li/37b29a88-d564-4ef6-9ab3-ff88954c87fc',
+        },
+      ],
+    },
   ],
 });
