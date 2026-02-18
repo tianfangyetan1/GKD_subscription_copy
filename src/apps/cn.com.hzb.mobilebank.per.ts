55d50b19ab6a0264045b4c42fa76fe7fd5830aec
天方夜谭 <tianfangyetan01@qq.com>
2026-02-18 17:58:53 +0800

    perf: 杭州银行-全屏广告

--- src/apps/cn.com.hzb.mobilebank.per.ts
+++ src/apps/cn.com.hzb.mobilebank.per.ts
@@ -11,6 +11,7 @@ export default defineGkdApp({
         {
           fastQuery: true,
           activityIds: 'cn.com.hzb.mobilebank.per.MainActivity',
+          matchRoot: true,
           matches:
             'Image[width<160 && height<160][childCount=0] < @View[clickable=true][childCount=1] <2 View[childCount=2] < View[index=parent.childCount.minus(1)][childCount=1] <n View[id="app"] <<3 [vid="h5_pc_container"]',
           snapshotUrls: [
