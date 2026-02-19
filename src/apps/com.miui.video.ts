dec366f9aba7049bcd0d862d3bc24bc8671a5d11
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-02-19 13:08:54 +0800

    feat: 小米视频 局部广告,分段广告-卡片广告 (#28)

--- src/apps/com.miui.video.ts
+++ src/apps/com.miui.video.ts
@@ -12,15 +12,33 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          activityIds: '.feature.detail.NewShortVideoDetailActivity',
+          activityIds: [
+            '.feature.detail.NewShortVideoDetailActivity',
+            '.feature.detail.NewLongVideoDetailActivityV2',
+          ],
           matches:
-            '[vid="pause_ad_close_img" || vid="iv_ad" || vid="v_banner_bottom_right"][clickable=true][visibleToUser=true]',
+            '[vid="pause_ad_close_img" || vid="iv_ad" || vid="v_banner_bottom_right" || vid="ad_close" || vid="v_banner_bottomright"][clickable=true]',
           exampleUrls:
             'https://m.gkd.li/57941037/cd25f4a5-64db-4471-8eea-3d84a34ffb72',
           snapshotUrls: [
-            'https://i.gkd.li/i/16044569',
-            'https://i.gkd.li/i/25323739',
+            'https://i.gkd.li/i/16044569', // iv_ad , v_banner_bottom_right
+            'https://i.gkd.li/i/25323739', // pause_ad_close_img
+            'https://i.gkd.li/i/25404935', // iv_ad , ad_close , v_banner_bottomright
+            'https://i.gkd.li/i/25404805', // v_banner_bottomright
           ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '局部广告',
+      desc: 'x掉',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.feature.main.MainTabActivity',
+          matches: '[vid="ad_action_close"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/25404970',
         },
       ],
     },
